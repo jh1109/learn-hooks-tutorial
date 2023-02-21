5c7768b25bdfa794871ed75228edf6cbd1e7c0bf
Juhee Noh <nohjuhee227@gmail.com>
2023-02-22 00:16:58 +0900

    [add] useState 객체로 활용하기

--- src/Info.js
+++ src/Info.js
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const [form, setForm] = useState({
+    name: "",
+    nickname: "",
+  });
+  const onChange = (e) => {
+    const nextForm = { ...form };
+    nextForm[e.target.name] = e.target.value;
+    setForm(nextForm);
   };
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={form.name} onChange={onChange} />
+        <input name="nickname" value={form.nickname} onChange={onChange} />
       </div>
       <div>
         <div>
-          <b>이름:</b> {name}
+          <b>이름:</b> {form.name}
         </div>
         <div>
-          <b>닉네임:</b> {nickname}
+          <b>닉네임:</b> {form.nickname}
         </div>
       </div>
     </div>
