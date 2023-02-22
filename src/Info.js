2ab5a647224d78632200ba3ab2e28325d157825e
Juhee Noh <nohjuhee227@gmail.com>
2023-02-22 17:49:31 +0900

    [learn] 8.7 커스텀 Hooks 만들기

--- src/Info.js
+++ src/Info.js
@@ -1,20 +1,11 @@
-import { useReducer } from "react";
+import useInputs from './useInputs';
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
-    name: "",
-    nickname: "",
+  const [state, onChange] = useInputs({
+    name: '',
+    nickname: '',
   });
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
   return (
     <div>
       <div>
